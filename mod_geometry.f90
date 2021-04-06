a99c58b5c1e84ac99d2f58992089fac4fa6140b6
Marco Bertini <m.bertini92@gmail.com>
2021-04-06 15:11:48 +0200

    Added GPLv3 license

--- mod_geometry.f90
+++ mod_geometry.f90
@@ -1,3 +1,20 @@
+!  Copyright (C) 2020-2021     Marco Bertini
+!  
+!  This file is part of neb.x.
+!  
+!  neb.x is free software: you can redistribute it and/or modify
+!  it under the terms of the GNU General Public License as published by
+!  the Free Software Foundation, either version 3 of the License, or
+!  (at your option) any later version.
+!  
+!  neb.x is distributed in the hope that it will be useful,
+!  but WITHOUT ANY WARRANTY; without even the implied warranty of
+!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!  GNU General Public License for more details.
+!  
+!  You should have received a copy of the GNU General Public License
+!  along with neb.x.  If not, see <https://www.gnu.org/licenses/>.
+
 module geometry
 
 #ifdef USE_MPI

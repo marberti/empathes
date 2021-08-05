b29a9f36f1fb5fd44fce7120384dc27a7e4cac3f
Marco Bertini <m.bertini92@gmail.com>
2021-08-05 16:39:38 +0200

    Release v1.0

--- main.f90
+++ main.f90
@@ -323,7 +323,7 @@ end subroutine write_help
 
 subroutine write_build_version()
   
-  character(*), parameter :: version_number = "v1.0-beta"
+  character(*), parameter :: version_number = "v1.0"
 
   integer                 :: out_stream
 #ifdef USE_MPI
